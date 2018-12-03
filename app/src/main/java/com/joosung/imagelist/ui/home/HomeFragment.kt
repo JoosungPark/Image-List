d8ac649a0a005eb40f71d52bf6cd886fc60f65c7
joosung.park <sdop25@gmail.com>
2018-12-03 23:48:08 +0900

    [android] home view model 작성중 - image server 연동

--- app/src/main/java/com/joosung/imagelist/ui/home/HomeFragment.kt
+++ app/src/main/java/com/joosung/imagelist/ui/home/HomeFragment.kt
@@ -1,7 +1,18 @@
 package com.joosung.imagelist.ui.home
 
+import com.joosung.imagelist.common.App
 import com.joosung.imagelist.common.BaseFragment
 
 class HomeFragment : BaseFragment() {
 
+    init {
+        val shared = App.get().shared!!
+        val server = HomeImageServer(shared)
+        homeViewModelFactory = HomeViewModel(shared, server, disposables)
+    }
+
+    companion object {
+        lateinit var homeViewModelFactory: HomeViewModel
+        fun newInstance() = HomeFragment()
+    }
 }
